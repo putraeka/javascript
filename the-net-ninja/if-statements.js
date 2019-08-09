237b4661865359c52441df61105c9bf64f539bd2
putraeka <gh@eka.gdn>
2019-08-09 15:57:58 +0700

    learn for, while loop, if, else if and else

---
+++ the-net-ninja/if-statements.js
@@ -0,0 +1,25 @@
+// if statements
+
+// const age = 30;
+// if(age > 20){
+//     console.log('You are over 20 years old');
+// }
+
+// const ninjas = ['shaun', 'ryu', 'chun-li', 'yoshi'];
+// if(ninjas.length > 3){
+//     console.log("that's a lot of ninjas")
+// }
+
+// else if statement
+
+const password = 'passwordsdalabladfka';
+
+if(password.length >= 12){
+    console.log('the password migth be strong');
+} else if(password.length >= 8){
+    console.log('that password is long enough');
+} else {
+    console.log('password is not long enough');
+}
+
+
---
+++ the-net-ninja/while-loop.js
@@ -0,0 +1,23 @@
+// While loop
+
+// let i = 0;
+
+// while(i < 5){
+//     console.log('in loop: ', i);
+//     i++;
+// }
+
+// const names =['shaun', 'mario', 'luigi'];
+// let i = 0;
+// while(i < names.length){
+//     console.log(names[i]);
+//     i++;
+// }
+
+// do while loop
+
+let i = 3;
+do{
+    console.log('val of i is : ', i);
+    i++;
+} while(i < 5);
